81671ad68a470542ff4f11279284eb26e1b97baf
anselmo1991 <freulein.sonya@gmail.com>
2024-04-22 15:17:05 +0200

    New project

--- tests/selectors.py
+++
@@ -1,4 +0,0 @@
-SELECTOR_FILTER_BY_FANTASY = '//a[text()="Фэнтези"]'
-SELECTOR_SORT_BY_COMMENTS = '//a[text()=" По количеству комментариев "]'
-SELECTOR_COUNTERS = '//div[contains(@class,"b-btn-mobile_none")]//div[@class="b-book_item__counters"]/div/div[2]//div[@class="_cnt"]'
-SELECTOR_BOOK_INFO = '//div[@class="b-book_item__info"]/div/div[2]'
