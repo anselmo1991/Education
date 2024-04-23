d69a51f529ea0db9e5bae9e0d8a83e51ef34c5bd
anselmo1991 <freulein.sonya@gmail.com>
2024-04-23 08:36:37 +0200

    New project

--- page_objects/search_results_page.py
+++ page_objects/search_results_page.py
@@ -12,20 +12,22 @@ def counters_list():
     all_counters = browser.all('//div[contains(@class,"b-btn-mobile_none")]//div['
                                '@class="b-book_item__counters"]/div/div[2]//div[@class="_cnt"]')
     all_counters.should(have.size(20))
+    return all_counters
 
 
 def books_list():
     books = browser.all('//div[@class="b-book_item__info"]/div/div[2]')
     books.should(have.size(20))
+    return books
 
 
-def check_comments_order():
-    comments_list = [int(element.get(query.text)) for element in all_counters]
+def check_comments_order(counters):
+    comments_list = [int(element.get(query.text)) for element in counters]
     comments_sorted = sorted(comments_list, reverse=True)
     assert comments_list == comments_sorted, "Error: lists do not match"
 
 
-def fantasy_check():
+def fantasy_check(books):
     for book_element in books:
         tags = list()
         for element in book_element.all('a'):
--- tests/test_lit.py
+++ tests/test_lit.py
@@ -3,14 +3,14 @@ from page_objects.search_results_page import range_comments, counters_list
 from page_objects.search_results_page import check_comments_order, books_list, fantasy_check
 
 
-def test_litgorod_sort_comments_by_desc(open_browser):
+def test_litgorod_sort_comments_by_desc():
     fantasy_category()
     range_comments()
-    counters_list()
-    check_comments_order()
+    counters = counters_list()
+    check_comments_order(counters)
 
 
 def test_litgorod_filter_by_fantasy():
     fantasy_category()
-    books_list()
-    fantasy_check()
+    books_lit = books_list()
+    fantasy_check(books_lit)
