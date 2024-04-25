086202628da8c4daac0cf399ea33cc57882001e7
anselmo1991 <freulein.sonya@gmail.com>
2024-04-25 15:34:10 +0200

    New project

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,18 @@
+import os
 import pytest
 from selene import browser
-
 from tests.constants import TEST_URL
-
-browser.config.driver_name = 'firefox'
 
 
 @pytest.fixture(autouse=True)
-def open_browser():
-    """
-    Open browser
-    """
-    browser.open(TEST_URL)
+def browser_instance():
+    default_browser = os.getenv("DEFAULT_BROWSER", "firefox")
+    browser.config.driver = default_browser
     yield browser
     browser.quit()
+
+
+def open_browser(browser_instance, browser_name=None):
+    browser.config.driver = browser_name
+    browser_instance.open(TEST_URL)
+
