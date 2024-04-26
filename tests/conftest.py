93c310546eb5b1b6c44f52f6a3030579ea62d877
anselmo1991 <freulein.sonya@gmail.com>
2024-04-26 13:33:24 +0200

    New project

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,23 @@ from tests.constants import TEST_URL
 
 
 # browser.config.driver_name = os.getenv("DEFAULT_BROWSER", "firefox")
-options = webdriver.ChromeOptions()
-browser.config.driver_options = options
-browser.config.driver_remote_url = 'http://localhost:4444'
+def choose_browser(browser_name):
+    if browser_name == 'chrome':
+        option = webdriver.ChromeOptions()
+    elif browser_name == 'firefox':
+        option = webdriver.FirefoxOptions()
+    elif browser_name == 'edge':
+        option = webdriver.EdgeOptions()
+    else:
+        raise ValueError("Unsupported browser name")
+
+    browser.config.driver_remote_url = 'http://localhost:444'
+    browser.config.driver_options = option
 
 
 @pytest.fixture(autouse=True)
-def open_browser():
-    """
-    Open browser
-    """
+def open_browser(browser_name):
+    choose_browser(browser_name)
     browser.open(TEST_URL)
     yield browser
     browser.quit()
