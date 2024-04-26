d09863a62d83ad97cf1538a3768321413924f58f
anselmo1991 <freulein.sonya@gmail.com>
2024-04-26 10:56:16 +0200

    New project

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,24 @@
-import os
 import pytest
 from selene import browser
+from selenium import webdriver
 from tests.constants import TEST_URL
 
 
+# browser.config.driver_name = os.getenv("DEFAULT_BROWSER", "firefox")
+def setup(self):
+    options = webdriver.ChromeOptions()
+    self.driver = webdriver.Remote(
+        command_executor="https://USERNAME:localhost",
+        options=options
+    )
+    self.driver.implicitly_wait(30)
+
+
 @pytest.fixture(autouse=True)
-def browser_instance():
-    default_browser = os.getenv("DEFAULT_BROWSER", "firefox")
-    browser.config.driver = default_browser
+def open_browser():
+    """
+    Open browser
+    """
+    browser.open(TEST_URL)
     yield browser
     browser.quit()
-
-
-def open_browser(browser_instance, browser_name=None):
-    browser.config.driver = browser_name
-    browser_instance.open(TEST_URL)
-
