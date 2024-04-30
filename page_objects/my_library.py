398403e5ae788bbcc08941f9cbc8e5eb44c27da4
anselmo1991 <freulein.sonya@gmail.com>
2024-04-30 11:15:40 +0200

    New project

--- page_objects/my_library.py
+++ page_objects/my_library.py
@@ -1,17 +1,16 @@
 import allure
-from selene.support.conditions import have, be
+from selene.support.conditions import be
 from selene.support.shared import browser
 
 book_search = browser.element("//div[@class='h2']//a[contains(text(), ' Сын маминой подруги')]")
-delete_button = browser.element(
-    "//div[@class='h2']//a[contains(text(), ' Сын маминой подруги')]//ancestor::div[@class='b-book_item__header']/following-sibling::div[2]//li[6]/a")
+delete_button = browser.element("//div[@class='h2']//a[contains(text(), ' Сын маминой подруги')]//ancestor::div[@class='b-book_item__header']/following-sibling::div[2]//div[@class='b-menu_drop active']/ul/li[6]/a")
 exit_menu_button = browser.element("//div[@class='b-menu_side']/ul/li[14]/a")
 exit_button = browser.element("//div[@class='col-6 mb-2']/a")
 
 
 @allure.step("Find the book in the library")
 def find_book_in_the_library():
-    assert book_search.should(have.size(1)), "Expected 1 book to be found in the library"
+    assert book_search.should(be.visible), "Expected 1 book to be found in the library"
 
 
 @allure.step("Delete the book from the library")
