1f5abe3b11e879a16a2919b0e75089d1fe086184
anselmo1991 <freulein.sonya@gmail.com>
2024-05-10 11:36:16 +0200

    New project

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,14 +6,24 @@ pipeline {
                 sh '/var/jenkins_home/venv/bin/pip install -r requirements.txt'
             }
         }
+        stage('Activate venv') {
+            steps {
+                sh 'source /var/jenkins_home/venv/bin/activate'
+            }
+        }
         stage('Run tests') {
             steps {
-                sh '/var/jenkins_home/venv/bin/python pytest --alluredir=allure_results -n auto --reruns 2 --clean-alluredir' //
+                sh '/var/jenkins_home/venv/bin/python pytest --alluredir=allure_results -n auto --reruns 2 --clean-alluredir'
             }
         }
         stage('Prepare test results') {
             steps {
-                sh 'allure serve allure_results' //
+                sh 'allure serve allure_results'
+            }
+        }
+        stage('Deactivate venv') {
+            steps {
+                sh 'source /var/jenkins_home/venv/bin/activate'
             }
         }
     }
