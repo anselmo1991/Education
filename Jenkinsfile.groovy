775444c6f896d61b53ee90bb6c7becf9e1f27240
anselmo1991 <freulein.sonya@gmail.com>
2024-05-10 11:58:03 +0200

    New project

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,29 +1,19 @@
 pipeline {
     agent any
     stages {
-        stage('Activate venv') {
-            steps {
-                sh '. /var/jenkins_home/venv/bin/activate'
-            }
-        }
         stage('Install dependencies') {
             steps {
-                sh 'pip install -r requirements.txt'
+                sh '/var/jenkins_home/venv/bin/pip install -r requirements.txt'
             }
         }
         stage('Run tests') {
             steps {
-                sh 'pytest --alluredir=allure_results -n auto --reruns 2 --clean-alluredir'
+                sh '/var/jenkins_home/venv/bin/pytest --alluredir=allure_results -n auto --reruns 2 --clean-alluredir'
             }
         }
         stage('Prepare test results') {
             steps {
                 sh 'allure serve allure_results'
-            }
-        }
-        stage('Deactivate venv') {
-            steps {
-                sh 'source /var/jenkins_home/venv/bin/activate'
             }
         }
     }
